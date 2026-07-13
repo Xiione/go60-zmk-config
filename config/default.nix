7e978775daf0f199c4c45ddea8dc7e15b2078846
Xiione <hamiltonlwang12@gmail.com>
2026-07-12 23:38:21 -0400

    feat: add raw HID support for layer visualization
    for use with MoergoLayerViz

--- config/default.nix
+++ config/default.nix
@@ -4,8 +4,18 @@
 
 let
   config = ./.;
+  hidVizModules = [
+    ../modules/zmk-raw-hid
+    ../modules/zmk-hid-viz
+  ];
 
-  go60_left  = firmware.zmk.override { board = "go60_lh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60_lh.conf"; };
+  go60_left  = firmware.zmk.override {
+    board = "go60_lh";
+    shield = "raw_hid_adapter";
+    keymap = "${config}/go60.keymap";
+    kconfig = "${config}/go60_lh.conf";
+    extraModules = hidVizModules;
+  };
   go60_right = firmware.zmk.override { board = "go60_rh"; keymap = "${config}/go60.keymap"; kconfig = "${config}/go60_rh.conf"; };
 
 in firmware.combine_uf2 go60_left go60_right "go60"
